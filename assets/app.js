98452624b9eeeed3512b556fe2e61258e5e2a582
kmajor1 <42553285+kmajor1@users.noreply.github.com>
2018-11-10 14:31:06 -0500

    added a base64 string conversion util

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,4 @@
-// Initialize Firebase
+// Configure Firebase
 var config = {
     apiKey: "AIzaSyA9l_Xn-60I1ullS7D3MwKxPFFHDmsibso",
     authDomain: "marvelherogenerator.firebaseapp.com",
@@ -7,30 +7,52 @@ var config = {
     storageBucket: "gs://marvelherogenerator.appspot.com",
     messagingSenderId: "317800982498"
 };
+
+// initialize firebase app
 firebase.initializeApp(config);
-// firebase auth 
+
+// firebase auth function 
 var auth = firebase.auth();
-// firebase storage 
-// Create a root reference
+
+// firebase storage references 
 var storageRef = firebase.storage().ref();
-var imageFile;
-var imageForUpload;
-var imageforFPP;
-// upload function 
-function uploadHandler(e) {
-    var uploadFile = e.target.files[0];
+
+// event handler function 
+// invoked after a change to the file selection 
+// only selects the first file selected by the user 
+
+function uploadHandler(evt) {
+    var files = evt.target.files;
+    var file = files[0];
+
+    // if files exist then load a filereader object, convert to binary string and store result 
+    if (files && file) {
+        var reader = new FileReader();
+        // nested function that reads and converts file after the filereader is done loading (onload)
+        reader.onload = function (readerEvt) {
+            var binaryString = readerEvt.target.result;
+            console.log(btoa(binaryString));
+        };
+        reader.readAsBinaryString(file);
+        // call a function that initiates ajax call
+    }
+    var uploadFile = evt.target.files[0];
     storageRef.child('img/' + uploadFile.name).put(uploadFile).then(
         storageRef.child('img/' + uploadFile.name).getDownloadURL().then(
-            function(url) {
-                console.log(url);      
-      }).then(function(t) {
-          console.log(t);
-      }))};
+            function (url) {
+                console.log(url);
+            }
+        )
+    )
+}
 
-    window.onload = function () {
-            document.getElementById('userImage').addEventListener('change', uploadHandler)
-        }
 
-        
+if (window.File && window.FileReader && window.FileList && window.Blob) {
+    document.getElementById('filePicker').addEventListener('change', uploadHandler, false);
+} else {
+    alert('The File APIs are not fully supported in this browser.');
+}
 
-    
+
+
+
