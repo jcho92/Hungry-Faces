3f763e444ff2ace80f7ff4f080917bd62a7b75d1
kmajor1 <42553285+kmajor1@users.noreply.github.com>
2018-11-14 12:46:20 -0500

    refined displayResults fn. removed unecessary files

--- assets/app.js
+++ assets/app.js
@@ -21,14 +21,16 @@ firebase.initializeApp(config);
 var auth = firebase.auth();
 
 // firebase storage references 
-var storageRef = firebase.storage().ref();
+var storageRef = firebase.storage().ref(); 
 
-
-// event handler function 
-// invoked after a change to the file selection 
-// only selects the first file selected by the user 
+// 
+$('body').on('load', 'img', function () {
+    console.log('this worked');
+    document.getElementsByTagName('img').classList = "";
+})
 
 // display results function 
+// shows image elements returned by APIs
 var displayResults = function (personImgUrl, marvelImgUrl) {
     console.log('calling display results');
     var marvelImgDiv = document.getElementById('marvelDiv');
@@ -36,13 +38,14 @@ var displayResults = function (personImgUrl, marvelImgUrl) {
     var personImgDiv = document.getElementById('personDiv');
     var personImgTag = document.createElement('img');
     marvelImgTag.src = marvelImgUrl;
-    marvelImgTag.classList = "resize";
+    marvelImgTag.classList = "resize d-none";
     personImgTag.src = personImgUrl;
-    personImgTag.classList = "resize";
+    personImgTag.classList = "resize d-none";
     marvelImgDiv.append(marvelImgTag);
     personImgDiv.append(personImgTag);
     // hide the main image 
     document.getElementById('mainSplashImg').classList = "marveluniverse d-none";
+    marvelImgTag.onload
 }
 
 // ajax call to face ++ 
